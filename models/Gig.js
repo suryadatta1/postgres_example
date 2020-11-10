46108ab19ae8d17a8e9a943f00da9ee4ebe945aa
surya <surya@aiphant.io>
2020-11-10 14:04:12 +0530

    basic Crud Operations Completed

--- models/Gig.js
+++ models/Gig.js
@@ -4,16 +4,13 @@ const db = require('../config/database');
 const Gig = db.define('gig', {
   name: {
     type: Sequelize.STRING,
-    allowNull: false,
   },
   email: {
     type: Sequelize.STRING,
-    allowNull: false,
   },
   userGroup: {
     type: Sequelize.STRING,
-    allowNull: false,
-  }
+  },
 });
 
 Gig.sync().then(() => {
